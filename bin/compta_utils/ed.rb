77727f1e7f08a0c2622eaf2771323ec8b88579a0
Mickael Riga <mickael.riga.79@gmail.com>
2023-02-21 15:58:34 +0100

    Handle total update in compta after an invoice/proposal was amended

--- bin/compta_utils/ed.rb
+++ bin/compta_utils/ed.rb
@@ -11,11 +11,15 @@ module Compta
 
   def ed_proposal number, year=Time.now.year
     ed( find_proposal( number, year ) )
+    edit_total_if_mismatch :proposal, number, year
+    $compta_return
   end
   alias_method :edp, :ed_proposal
 
   def ed_invoice number, year=Time.now.year
     ed( find_invoice( number, year ) )
+    edit_total_if_mismatch :invoice, number, year
+    $compta_return
   end
   alias_method :edi, :ed_invoice
 
@@ -31,6 +35,25 @@ module Compta
       puts "File #{ file } does not exist.".red
     end
     $compta_return
+  end
+
+  private
+
+  def edit_total_if_mismatch type, number, year=Time.now.year
+    # Offers to update the total if not equal to total item prices.
+    # Presumably after a manual update of the items.
+    if type != :invoice and type != :proposal
+      puts "No total calculation for type #{ type }.".red
+      return $compta_return
+    end
+    doc = load_doc type, number, year
+    total = ( doc[:items] or [] ).inject( 0 ) do |sum, item|
+      sum + item[:price]
+    end
+    if doc[:total] != total and confirm( "Total is not up-to-date. Would you like to update it?".yellow )
+      doc[:total] = total
+      save_doc type, doc
+    end
   end
 
 end
