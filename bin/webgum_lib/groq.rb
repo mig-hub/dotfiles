2890dc1bc8bd67e16064ff1de670dead6a28d8c2
Mickael Riga <mickael.riga.79@gmail.com>
2025-10-21 11:47:14 +0200

    Add webgum groq methods

--- bin/webgum_lib/groq.rb
+++ bin/webgum_lib/groq.rb
@@ -7,6 +7,47 @@ class Webgum < Thor
 
     include Utils
 
+    desc "schemas", "List all schema GROQ field definitions"
+    def schemas
+      config['schemas'].each do |s|
+        schema(s['name'])
+      end
+    end
+
+    desc "schema SCHEMA_NAME", "List all GROQ fields in the specified schema"
+    def schema(schema_name)
+      s = get_schema(schema_name)
+      puts "const #{s['name']}Fields = `"
+      puts "  ...,"
+      s['fields'].each do |f|
+        if f['type'] == 'image' or f['type'] == 'file'
+          puts "  \"#{f['name']}Url\": #{f['name']}.asset->url,"
+        elsif f['type'] == 'reference'
+          puts "  \"#{f['name']}\": #{f['name']}->{"
+          f['to'].each do |to_item|
+            puts "    _type == #{ to_item['type'] } => @{ ${#{ to_item['type'] }Fields} },"
+          end
+          puts "  },"
+        elsif f['type'] == 'array'
+          puts "  \"#{f['name']}\": #{f['name']}[]{"
+          f['of'].each do |of_item|
+            if of_item['type'] == 'reference'
+              puts "    _type == 'reference' => @->{"
+              of_item['to'].each do |to_item|
+                puts "      _type == #{ to_item['type'] } => @{ ${#{ to_item['type'] }Fields} },"
+              end
+              puts "    },"
+            else
+              puts "    _type == #{ of_item['type'] } => @{ ${#{ of_item['type'] }Fields} },"
+            end
+          end
+          puts "  },"
+        end
+      end
+      puts "`;"
+      puts
+    end
+
   end
 end
 
